d77594db585b3c98e0796c6cdc2c3260aed7f397
John Smith <you@example.com>
2023-12-21 22:53:29 +0100

    overwrite existing files on unstow

--- bin/bin/bootstrap.sh
+++ bin/bin/bootstrap.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
 
-git clone https://github.com/kovan/dotfiles.git
-cd dotfiles
-git remote set-url origin "git@github.com:kovan/dotfiles.git"
-cd ..
 
 #git clone --depth 1 --single-branch https://github.com/doomemacs/doomemacs ~/.config/emacs
 #~/.config/emacs/bin/doom install
@@ -15,10 +11,15 @@ else
 	INSTALL_CMD="apt install -y"
 fi
 
-for pkg in fish stow curl wget tmux git fzf nvim net-tools apt-file netcat strace ltrace bwm-ng ripgrep htop fd-find aptitude bpytop rsync lsb-release gnupg ca-certificates lsof
+for pkg in fish stow curl wget tmux git fzf nvim net-tools apt-file netcat strace ltrace bwm-ng ripgrep htop fd-find aptitude bpytop rsync lsb-release gnupg ca-certificates lsof make
 do
 	sudo $INSTALL_CMD $pkg
 done
+
+
+cd dotfiles
+git remote set-url origin "git@github.com:kovan/dotfiles.git"
+make
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
