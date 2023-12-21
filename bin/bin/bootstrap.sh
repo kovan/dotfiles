2ffc3f5d3041688e1f84155a47c34865cf17bdcc
John Smith <you@example.com>
2023-12-21 23:04:27 +0100

    bin/bin/bootstrap.sh

--- bin/bin/bootstrap.sh
+++ bin/bin/bootstrap.sh
@@ -11,7 +11,7 @@ else
 	INSTALL_CMD="apt install -y"
 fi
 
-for pkg in fish stow curl wget tmux git fzf nvim net-tools apt-file netcat strace ltrace bwm-ng ripgrep htop fd-find aptitude bpytop rsync lsb-release gnupg ca-certificates lsof make
+for pkg in fish stow curl wget tmux git fzf nvim neovim net-tools apt-file netcat strace ltrace bwm-ng ripgrep htop fd-find aptitude bpytop rsync lsb-release gnupg ca-certificates lsof make
 do
 	sudo $INSTALL_CMD $pkg
 done
@@ -22,6 +22,8 @@ git remote set-url origin "git@github.com:kovan/dotfiles.git"
 make
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+chsh -s /usr/bin/fish
 
 omf install https://github.com/jhillyerd/plugin-git
 omf install https://github.com/jethrokuan/fzf
