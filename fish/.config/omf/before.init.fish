af1b26100c9ce974886ca37e98f9056da51aaf05
John Smith <you@example.com>
2023-12-31 22:21:14 +0100

    asdf

--- fish/.config/omf/before.init.fish
+++ fish/.config/omf/before.init.fish
@@ -8,6 +8,8 @@ function fuck -d "Correct your previous console command"
   end
 end
 
+fish_config theme choose Dracula
+
 alias lola "git log --graph --decorate --pretty=oneline --abbrev-commit"
 alias drop_cache "sudo sh -c \"echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'\""
 alias vi nvim
